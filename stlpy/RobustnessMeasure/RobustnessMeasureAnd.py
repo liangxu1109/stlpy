4ed85e6042e495389b1893da0415061cffd6fe36
Xu Liang <xu.liang@tum.de>
2022-06-27 21:44:32 +0200

    add Newrobustness metrices to the cost function

--- stlpy/RobustnessMeasure/RobustnessMeasureAnd.py
+++ stlpy/RobustnessMeasure/RobustnessMeasureAnd.py
@@ -91,7 +91,21 @@ class RobustnessMeasure_and():
         return out
 
     def NewRobustness(self, y, t, robustness_type):
-        pass
+        list = ([formula.robustness(y, t + self.timesteps[i], robustness_type) for i, formula in
+                 enumerate(self.subformula_list)])  # all robustness in a entirely encoded STL
+        if any(list[i] <= 0 for i in range(len(list))):
+            list1 = []  # list which is calculated, only choose the negative robustness
+            for i in range(len(list)):
+                if list[i] <= 0:
+                    list1.append(list[i])
+            out = (sum(list1) / len(list))
+        else:
+            out = list[0] + 1  #
+            for i in range(1, len(list)):
+                out *= (list[i] + 1)
+            out = math.pow(out, 1 / len(list)) - 1
+        return out
+
 
     def TimeRobustness(self, y, t, robustness_type):
         pass
